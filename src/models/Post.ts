f43e217dbdda5469abcf2843625c526471328e03
falcaomm <manu.m.falcao@gmail.com>
2023-06-03 00:34:21 -0300

    endpoitns parte1

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -8,6 +8,17 @@ export interface PostDB {
     updated_at: string
 }
 
+export interface PostDBWithCreatorName {
+    id: string,
+    creator_id: string,
+    content: string,
+    likes: number,
+    dislikes: number,
+    created_at: string,
+    updated_at: string,
+    creator_name: string
+}
+
 export interface PostModel {
     id: string,
     content: string,
@@ -24,13 +35,13 @@ export interface PostModel {
 export class Post {
     constructor(
         private id: string,
-        private creatorId: string,
-        private creatorName: string,
         private content: string,
         private likes: number,
         private dislikes: number,
         private createdAt: string,
-        private updatedAt: string
+        private updatedAt: string,
+        private creatorId: string,
+        private creatorName: string
     ) {}
 
     public getId(): string {
